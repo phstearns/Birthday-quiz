45a67a05bef5d22e0faf6e8d7c0df5da12088128
phstearns <phstearns13@gmail.com>
2016-02-02 14:51:12 -0500

    Updated from Brython Server: 2/2/2016 2:51:12 PM

--- birthday.py
+++ birthday.py
@@ -53,10 +53,10 @@ print(season)
 
 if y is [1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989]:
     type = ("eighties")
-elif y>= 1990 or y<= 1999:
+elif y is [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]:
     type = ("nineties")
-elif y>= 2000:
+elif y > 2000:
     type = ("two thousands")
-elif y<= 1980:
+elif y < 1980:
     type = ("Stone Age")
 print(type)
